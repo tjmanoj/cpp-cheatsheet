7b3566c4b60bb9badc6659036d1d1917a89d5655
Manoj T <100189789+tjmanoj@users.noreply.github.com>
2024-04-19 14:38:06 +0530

    Update 006 static member.cpp

--- 006 static member.cpp
+++ 006 static member.cpp
@@ -10,26 +10,12 @@ public:
 		count = 10;
 	}
 	static int count;
-	};
-int Base:: count = 0;			// need declaration globally to access all
 	
-int main() {
-  Base a2(19);
-  cout << a2.count << endl;
-  cout << Base::count << endl;
-  return 0;
-}#include<bits/stdc++.h>
-using namespace std;
-
-class Base{
-private:
-	int a;
-public: 
-	Base(int val) { 
-		a= val;
-		count = 10;
+	// static member function
+	static int getData(){           //only use the static variable inside function
+	    count++;
+	    return count;
 	}
-	static int count;
 	};
 int Base:: count = 0;			// need declaration globally to access all
 	
@@ -37,5 +23,8 @@ int main() {
   Base a2(19);
   cout << a2.count << endl;
   cout << Base::count << endl;
+  
+  cout << a2.getData() << endl;
+  cout << Base::getData();
   return 0;
 }
